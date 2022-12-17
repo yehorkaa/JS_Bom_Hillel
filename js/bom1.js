ddd1f4c581c60fde461c0a172844e1ef185b406d
Yehor <107763117+yehorkaa@users.noreply.github.com>
2022-12-17 13:31:35 +0200

    new

--- js/bom1.js
+++ js/bom1.js
@@ -1,29 +1,30 @@
-const btn = document.querySelector('#login')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-
+const login = document.querySelector("#login");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const menu = document.querySelector(".menu")
 
 let value1 = password;
 let value2 = email;
-console.log(btn)
+console.log(login);
+
+login.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(value1.value);
+  console.log(value2.value);
+  menu.style.display = "none";
+  login.style.display = "none";
+  email.style.display = "none";
+  password.style.display = "none";
+  document.querySelector(".spinner").style.display = "block";
+  setTimeout(
+    () => (window.location.href = "../js_spinner/page/indexSecond.html"),
+    2000
+  );
 
 
-btn.addEventListener('click', function(event){
-    event.preventDefault()
-    console.log(value1.value)
-    console.log(value2.value)
-     setTimeout(function(){ open(event.target.href = 'file:///C:/Frontend_pr/2022/JS_hillel_spinner/js_spinner/page/indexSecond.html', '_blank')}, 2000);
-    btn.style.display = 'none'
-    email.style.display = 'none'
-    password.style.display = 'none'
-    // document.querySelectorAll('input').style.display = 'none';
-    let variable  = setTimeout(document.querySelector('.spinner').style.display = 'block', 0)
-    clearTimeout(variable)
-    // let timer =
-    // clearTimeout(timer)
-})
+});
 
+// const btn = document.querySelector('#btn')
+// console.log(btn)
 
-
-
-// 
+//
--- js/bom2.js
+++ js/bom2.js
@@ -1,5 +1,23 @@
-console.log('width: ', screen.width)
-console.log('height: ', screen.height)
 
+  console.log("width: ", screen.width);
+  console.log("height: ", screen.height);
+
+  const successCallback = (position) => {
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", function (event) {
+  
+    event.preventDefault();
+    history.back();
+  });
 
 
